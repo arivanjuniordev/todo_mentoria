b76d1684bb4d76d42ce33a24066ebe11e0f47af5
arivanjuniordev <94536010+arivanjuniordev@users.noreply.github.com>
2025-02-03 11:21:21 -0300

    swagger

--- src/todo/dto/create-todo.dto.ts
+++ src/todo/dto/create-todo.dto.ts
@@ -1,3 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateTodoDto {
+  @ApiProperty({ description: 'The title of the todo item' })
   title: string;
+
+  @ApiProperty({ description: 'Status do ToDo', example: false })
+  completed: boolean;
 }
--- src/todo/dto/update-todo.dto.ts
+++ src/todo/dto/update-todo.dto.ts
@@ -1,4 +1,17 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTodoDto } from './create-todo.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateTodoDto extends PartialType(CreateTodoDto) {}
+export class UpdateTodoDto extends PartialType(CreateTodoDto) {
+  @ApiProperty({
+    description: 'The description of the todo item',
+    required: false,
+  })
+  title?: string;
+
+  @ApiProperty({
+    description: 'The completion status of the todo item',
+    required: false,
+  })
+  completed?: boolean;
+}
